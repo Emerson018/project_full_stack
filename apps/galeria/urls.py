96f73ab60a7ad1d31ee3aa23a8f7b6154a0dc945
Emerson Viçosa <emersonvicosa@gmail.com>
2023-10-19 22:14:49 -0300

    add novo icone

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,12 +1,14 @@
 #URLS DA GALERIA
-
 from  django.urls import path
-from apps.galeria.views import index, imagem, buscar
+from apps.galeria.views import \
+    index, imagem, buscar, nova_imagem,editar_imagem,deletar_imagem
 
 #boas praticas. aqui só tem URLS se essas paginas forem relacionadas a GALERIA.
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path("buscar", buscar, name="buscar"),
-
-]
+    path("nova-imagem", nova_imagem, name="nova_imagem"),
+    path("editar-imagem", editar_imagem, name="editar_imagem"),
+    path("deletar-imagem", deletar_imagem, name="deletar_imagem"),
+]   
